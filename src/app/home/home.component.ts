64682af9d44c29c0f533d4d60e664debf6f5a95a
fer161 <142939646+fer161@users.noreply.github.com>
2023-10-04 20:33:36 -0600

    FerSilva

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { ApiRestService } from '../api-rest.service';
+import { subscribeOn } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
+//Autor Fernanda Mendiola Silva
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+preguntas = [
+  {no: 1, pregunta: 'No hay', correo:"", categoria: "", fecha: "", id:""},
+];
+newP = {categoria:"", pregunta:""}
+mod = {categoria:"", pregunta: "", id:""}
+
+constructor(
+  private api: ApiRestService, 
+  private msg: ToastrService){}
+
+ngOnInit(): void{
+  this.consultar()
+}
+consultar(){
+  this.api.getAllPreguntas().subscribe({
+    next: datos => {
+      console.log(datos)
+      let documentos = datos.documents.filter((d:any) => d.hasOwnProperty('fields'))
+      let i = 1;
+      let preguntas = documentos.map( (p:{name:String,fields:any}) => ({
+        no: i++,
+        pregunta: p.fields.hasOwnProperty('pregunta')? p.fields.pregunta.stringValue: "",
+        correo: p.fields.hasOwnProperty('correo')? p.fields.correo.stringValue: "",
+        categoria: p.fields.hasOwnProperty('categoria')? p.fields.categoria.stringValue: "",
+        fecha: p.fields.hasOwnProperty('fecha')? p.fields.fecha.timestampValue: "",
+        id: p.name.split("/").pop()
+      }))
+      console.log(preguntas)
+      this.preguntas = preguntas
+    },
+    error: e => {}
+  })
+}
+
+//Autor Fernanda Mendiola Silva
+
+crearPregunta(){
+  const fecha = new Date().toISOString();
+  if(this.newP.pregunta=="" || this.newP.categoria==""){
+    this.msg.error("Falta llenar los datos")
+    return;
+}
+const correo = localStorage.getItem("correo") || ""
+this.api.createPregunta(this.newP.pregunta, correo, this.newP.categoria, fecha).subscribe({
+next: resp => {this.consultar(); this.msg.success("Pregunta creada")},
+error: e => {console.log(e); this.msg.error("Error al crear la pregunta")}
+})
+
+}
+eliminarPregunta(id:string){
+  this.api.deletePregunta(id).subscribe({
+    next: resp => {this.consultar(), this.msg.success("Pregunta eliminada")},
+    error: e => {console.log(e)}
+  })
+}
+
+modificarPregunta(){
+  this.api.updatePregunta(this.mod.pregunta, this.mod.id).subscribe({
+    next: resp => {this.consultar(), this.msg.success("Pregunta modificada")},
+    error: e => {console.log(e)}
+  })
+}
+editarPregunta(p:any){
+  this.mod = JSON.parse(JSON.stringify(p))
+}
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiRestService } from '../api-rest.service';
+import { ToastrService } from 'ngx-toastr';
+
+//Autor Fernanda Mendiola Silva
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = ""
+  pass = ""
+  showError = false
+  showLoading = false
+constructor(
+  private router: Router, 
+  private api: ApiRestService,
+  private msg:ToastrService
+  ){}
+login(){
+  this.showLoading = true
+  this.api.login(this.email, this.pass).subscribe({
+    next: bien => {
+      this.msg.success("Bienvenido a mi foro")
+      localStorage.setItem("correo", this.email);
+      this.router.navigate(['/home']); 
+    },
+    error: mal => {
+      this.msg.error("Error de usuario o contraseña")
+      this.showError = true
+      this.showLoading = false
+    }
+});
+ 
+}
+
+}
